526fde9c822ce69388023ff511d179915a6e0c53
Guilherme R. Missel <gmissel09@gmail.com>
2026-08-21 21:32:39 -0300

    feat: adiciona Periodico e Main de teste

--- lista-avaliativa/src/biblioteca/Periodico.java
+++ lista-avaliativa/src/biblioteca/Periodico.java
@@ -1,4 +1,14 @@
 package biblioteca;
 
-public class Periodico {
+public class Periodico extends Publicacao {
+  private int numeroVolume;
+
+  public Periodico(String titulo, int numeroVolume) {
+    super(titulo);
+    this.numeroVolume = numeroVolume;
+  }
+
+  public int getNumeroVolume() {
+    return numeroVolume;
+  }
 }
